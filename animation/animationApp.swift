ac62e90762c60198136ffcabaf6db75044251a24
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-07-25 12:57:44 +0300

    Initial Commit

---
+++ animation/animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationApp.swift
+//  animation
+//
+//  Created by Feyzullah Durası on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
